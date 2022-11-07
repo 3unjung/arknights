2e947c83b25ae742b3e593f63ceb61e2fb9eaa09
3unjung <margaux.aclo1941@gmail.com>
2022-11-07 11:58:27 +0100

    isPressed bool work correctly

--- lib/screens/ressource_tier.dart
+++ lib/screens/ressource_tier.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 class RessourceTier extends StatefulWidget {
   final List tierData;
 
-
   const RessourceTier({super.key, required this.tierData});
 
   @override
@@ -14,47 +13,37 @@ class RessourceTier extends StatefulWidget {
 
 class _RessourceTierState extends State<RessourceTier> {
   final noLmd = res[0];
+  bool isPressed = false;
   @override
   Widget build(BuildContext context) {
     List<Widget> tierWidgets = [];
     final noLmd = widget.tierData;
-    bool isPressed = false;
-
-    late Text ite;
-    late Text zed;
-
-
-
 
     if(noLmd.length>1){
-      //
-      // if (isPressed == true) {
-      //  ite =  Text("je suis vraie");
-      // }
-      // else { zed = Text("je suis mauvaise");}
-      //
-      // // (isPressed == true) ? ite = Text("je suis vraie") :  zed = Text("je suis mauvaise");
-
 
       for(Map tierItem in widget.tierData) {
+        debugPrint("isPressed $isPressed");
         tierWidgets.add(
 
           Row(
             children: [
               Image.asset(tierItem["img"],width: 90,height: 45,),
               IconButton(onPressed: (){
-                setState(() { isPressed == true; tierItem["qty"] += 1 ;});},
-                icon: const Icon(Icons.arrow_upward, size: 35, color: Colors.blue,),
-                  // Text("${tierItem["qty"]}", style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w900);
+                setState(() {
+                  isPressed = true;
+                  tierItem["qty"] += 1;
+                });},
 
+                icon: const Icon(Icons.arrow_upward, size: 35, color: Colors.blue,),
                   ),
 
-              // ite,
 
+              if (isPressed!) Text('${tierItem["qty"]}', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w900),),
 
-              if (isPressed == true) Text('${tierItem["qty"]}', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w900),) else const Text("chouette"),
-              
-              IconButton(onPressed: () {setState(() {tierItem["qty"] -= 1;});
+              IconButton(onPressed: () {setState(() {
+                tierItem["qty"] -= 1;
+
+              });
               },
                 icon: const Icon(
                   Icons.arrow_downward,
